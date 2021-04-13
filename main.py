732a9d4aebfb1badb0fc0a7d5ddef874627b9a31
firstName lastName <>
2021-04-13 00:51:29 +0000

    main.py was restarted e command and on_ready event were added

--- main.py
+++ main.py
@@ -1,24 +1,19 @@
-# pog bot has been started nice
-
 import discord
 from discord.ext import commands
 import os
-intents = discord.Intents()
-intents.guilds = True
-intents.members = True
-client = commands.Bot(command_prefix=">>", intents = intents, case_insensitive = True)
-TOKEN = os.environ.get("TOKEN")
+
+client = commands.Bot(command_prefix=".")
+token = os.environ.get("TOKEN")
 
 @client.event
 async def on_ready():
-    print("serverBot online")
-    channel1 = client.get_channel(830120041705635840)
-    await channel1.send("Wassup, ive become online")
+  print("ready")
+  channel = await client.fetch_channel(830120041705635840)
+  await channel.send("ready")
 
-@client.command(name="test", description = "its a test")
-async def test(ctx):
-    print("test")
-    await ctx.delete()
-    await ctx.send("test")
+@client.command(name="e")
+async def e(ctx):
+  print("e")
+  await ctx.send("e")
 
-client.run(TOKEN)
+client.run(token)
